564877f7fffe203b63e006f1ced69c10f23bfc8d
3pple10 <mdahmed01600@gmail.com>
2025-07-31 14:48:26 +0600

    Initial commit

---
+++ src/main/java/com/example/bercportal_2311991_2420585_2420808_2420953/ConsumerApplyForNewConnectionController.java
@@ -0,0 +1,33 @@
+package com.example.bercportal_2311991_2420585_2420808_2420953;
+
+import javafx.event.ActionEvent;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+
+public class ConsumerApplyForNewConnectionController
+{
+    @javafx.fxml.FXML
+    private ImageView photo_4_FXID;
+    @javafx.fxml.FXML
+    private ImageView photo_3_FXID;
+    @javafx.fxml.FXML
+    private Text addPhotoFXID;
+    @javafx.fxml.FXML
+    private ImageView photo_2_FXID;
+    @javafx.fxml.FXML
+    private ImageView photo_5_FXID;
+    @javafx.fxml.FXML
+    private ImageView photo_1_FXID;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void ApplyForNewConnectionOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void BackToDashboardOnAction(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/com/example/bercportal_2311991_2420585_2420808_2420953/TariffApCoDashboard01Controller.java
@@ -0,0 +1,48 @@
+package com.example.bercportal_2311991_2420585_2420808_2420953;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
+public class TariffApCoDashboard01Controller
+{
+    @javafx.fxml.FXML
+    private Label nameShowLabelFXID;
+    @javafx.fxml.FXML
+    private Label idShowLabelFXID;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void HistoryOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void IncompleteProposalsOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void ProposalDetailedFilesOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void DeadlineAlertsOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void ReviewandAssignOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void AssignedProposalsOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void ForwardProposalOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void MyProposalsOnAction(ActionEvent actionEvent) {
+    }
+}
