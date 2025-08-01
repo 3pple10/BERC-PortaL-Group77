517f7f76eb4e6fa0f7ed7fd0a1a8b27118107263
3pple10 <mdahmed01600@gmail.com>
2025-08-02 00:16:27 +0600

    Initial commit

---
+++ src/main/java/com/example/bercportal_2311991_2420585_2420808_2420953/Card.java
@@ -0,0 +1,4 @@
+package com.example.bercportal_2311991_2420585_2420808_2420953;
+
+public class Card {
+}
---
+++ src/main/java/com/example/bercportal_2311991_2420585_2420808_2420953/Consumer.java
@@ -0,0 +1,60 @@
+package com.example.bercportal_2311991_2420585_2420808_2420953;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class Consumer extends UserSuperClass implements Serializable {
+    private final String consumerID;
+    private final long nidNo;
+    private final String cardID;
+    private final String billID;
+    private ArrayList <Bill> bills;
+    private ArrayList <Card> cards;
+    private ArrayList <MeterReading> meterReadings;
+    private ArrayList <Complaint> complaints ;
+    private ArrayList <Payment> payments;
+
+    public Consumer(String password, String name, String contactNo, String email, boolean isActive, LocalDate dateOfBirth, LocalDateTime lastLogin, String consumerID, long nidNo, String cardID, String billID) {
+        super(password, name, contactNo, email, isActive, dateOfBirth, lastLogin);
+        this.consumerID = consumerID;
+        this.nidNo = nidNo;
+        this.cardID = cardID;
+        this.billID = billID;
+    }
+
+    private String validateConsumerID(String consumerID){
+        if (consumerID == null || consumerID.length()==7){
+            throw new IllegalArgumentException("Consumer iD must be exactly 7 digits ");
+        }return consumerID;
+    }
+
+    @Override
+    public boolean updateProfile() {
+        return true;
+    }
+
+    @Override
+    public boolean login() {
+        this.setLastLogin(LocalDateTime.now());
+        this.setActive(true);
+        return true;
+    }
+
+    @Override
+    public String userRolesType() {
+        return "";
+    }
+
+    @Override
+    public boolean logout() {
+        this.setActive(false);
+        return true;
+    }
+
+    @Override
+    public boolean signup() {
+        return true;
+    }
+}
