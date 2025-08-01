c5c052ffb098371d3eff00dff5388386cf936470
3pple10 <mdahmed01600@gmail.com>
2025-08-02 00:16:27 +0600

    Initial commit

---
+++ src/main/java/com/example/bercportal_2311991_2420585_2420808_2420953/ProccessSignupController.java
@@ -0,0 +1,29 @@
+package com.example.bercportal_2311991_2420585_2420808_2420953;
+
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.scene.control.TextField;
+
+public class ProccessSignupController
+{
+    @javafx.fxml.FXML
+    private TextField userIDSignupTextFieldFXID;
+    @javafx.fxml.FXML
+    private TextField emailSignupTextFieldFXID;
+    @javafx.fxml.FXML
+    private TextField userPasswordSignupTextFieldFXID1;
+    @javafx.fxml.FXML
+    private TextField reWriteUserPasswordSignupTextFieldFXID11;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void SignUpOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void LoginPageFXMLOnAction(Event event) {
+    }
+}
---
+++ src/main/java/com/example/bercportal_2311991_2420585_2420808_2420953/ProcessLoginController.java
@@ -0,0 +1,25 @@
+package com.example.bercportal_2311991_2420585_2420808_2420953;
+
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.scene.control.TextField;
+
+public class ProcessLoginController
+{
+    @javafx.fxml.FXML
+    private TextField userPasswordLoginTextFieldFXID;
+    @javafx.fxml.FXML
+    private TextField userIDLoginTextFieldFXID;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void SignUpPageFXMLOnAction(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void LoginOnAction(ActionEvent actionEvent) {
+    }
+}
